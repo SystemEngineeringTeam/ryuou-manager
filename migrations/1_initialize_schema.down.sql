26d34fddf6093f68d9d88b4ba19afcd3143bd072
SuperTikuwa <tikuwamk2@gmail.com>
2021-10-12 00:45:19 +0900

    fix:migrationが通らなくなっていたので修正

--- migrations/1_initialize_schema.down.sql
+++ migrations/1_initialize_schema.down.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `questions`;
-DROP TABLE IF EXISTS `teams`;
-DROP TABLE IF EXISTS `users`;
 DROP TABLE IF EXISTS `team_members`;
 DROP TABLE IF EXISTS `team_opened_questions`;
+DROP TABLE IF EXISTS `teams`;
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `questions`;
--- migrations/2_insert_testdata.up.sql
+++ migrations/2_insert_testdata.up.sql
@@ -1,8 +1,8 @@
-INSERT INTO `questions` ( `title`, `description`)
-VALUES ('Test Question 1', 'Test Question 1 Description');
+INSERT INTO `questions` (`title`, `description`, `score`)
+VALUES ('Test Question 1', 'Test Question 1 Description',1);
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`)
-VALUES (`1`,`Foo`,`foo@foo.com`,`hogehoge`);
+INSERT INTO `users` (`name`, `email`, `password`)
+VALUES ('Foo','foo@foo.com','hogehoge');
 
 INSERT INTO `teams` (`name`)
 VALUES ('Team 1');
@@ -10,7 +10,7 @@ VALUES ('Team 1');
 INSERT INTO `team_members` (`team_id`, `user_id`)
 VALUES (1, 1);
 
-INSERT INTO `team_open_questions` (`team_id`, `question_id`)
+INSERT INTO `team_opened_questions` (`team_id`, `question_id`)
 VALUES (1, 1);
 
 
