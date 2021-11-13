cf11f311a5081c14f9654a675c79d45ea665dae2
SuperTikuwa <tikuwamk2@gmail.com>
2021-11-13 17:41:39 +0900

    [from now] 2021/11/13 17:41:39
    diff --git a/backend/go.mod b/backend/go.mod
    index 9111ad5..5a6c4db 100644
    --- a/backend/go.mod
    +++ b/backend/go.mod
    @@ -2,7 +2,10 @@ module github.com/SystemEngineeringTeam/ryuou-manager
    
     go 1.17
    
    -require github.com/jinzhu/gorm v1.9.16
    +require (
    +	github.com/gorilla/mux v1.8.0
    +	github.com/jinzhu/gorm v1.9.16
    +)
    
     require (
     	github.com/go-sql-driver/mysql v1.5.0 // indirect
    diff --git a/backend/go.sum b/backend/go.sum
    index acd4c01..32e2884 100644
    --- a/backend/go.sum
    +++ b/backend/go.sum
    @@ -8,6 +8,8 @@ github.com/go-sql-driver/mysql v1.5.0 h1:ozyZYNQW3x3HtqT1jira07DN2PArx2v7/mN66gG
     github.com/go-sql-driver/mysql v1.5.0/go.mod h1:DCzpHaOWr8IXmIStZouvnhqoel9Qv2LBy8hT2VhHyBg=
     github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe h1:lXe2qZdvpiX5WZkZR4hgp4KJVfY3nMkvmwbVkpv1rVY=
     github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe/go.mod h1:8vg3r2VgvsThLBIFL93Qb5yWzgyZWhEmBwUJWevAkK0=
    +github.com/gorilla/mux v1.8.0 h1:i40aqfkR1h2SlN9hojwV5ZA91wcXFOvkdNIeFDP5koI=
    +github.com/gorilla/mux v1.8.0/go.mod h1:DVbg23sWSpFRCP0SfiEN6jmj59UnW/n46BH5rLB71So=
     github.com/jinzhu/gorm v1.9.16 h1:+IyIjPEABKRpsu/F8OvDPy9fyQlgsg2luMV2ZIH5i5o=
     github.com/jinzhu/gorm v1.9.16/go.mod h1:G3LB3wezTOWM2ITLzPxEXgSkOXAntiLHS7UdBefADcs=
     github.com/jinzhu/inflection v1.0.0 h1:K317FqzuhWc8YvSVlFMCCUb36O/S9MCKRDI7QkRKD/E=
    diff --git a/backend/main.go b/backend/main.go
    index 2300dfd..8b5ccc8 100644
    --- a/backend/main.go
    +++ b/backend/main.go
    @@ -3,15 +3,29 @@ package main
     import (
     	"net/http"
    
    -	"github.com/SystemEngineeringTeam/ryuou-manager/handler"
    +	"github.com/gorilla/mux"
    +	// "github.com/SystemEngineeringTeam/ryuou-manager/handler"
     )
    
     func main() {
    -	http.HandleFunc("/questions", handler.QuestionHandler)
    -	http.HandleFunc("/admin/questions", handler.AdminQuestionHandler)
    -	http.HandleFunc("/admin/teams", handler.AdminTeamHandler)
    -	http.HandleFunc("/users", handler.UserHandler)
    -	http.HandleFunc("/users/login", handler.LoginHandler)
    +	r := mux.NewRouter()
    +
    +	// /questions
    +	r.HandleFunc("/questions/{team_id:[0-9]+}", nil)
    +	r.HandleFunc("/questions/{team_id:[0-9]+}/{question_id:[0-9]+}", nil)
    +
    +	// /users
    +	r.HandleFunc("/users", nil)
    +	r.HandleFunc("/users/login", nil)
    +
    +	// /admin/questions
    +	r.HandleFunc("/admin/questions", nil)
    +	r.HandleFunc("/admin/questions/{question_id:[0-9]+}", nil)
    +	r.HandleFunc("/admin/questions/{team_id:[0-9]+}/{question_id:[0-9]+}", nil)
    +
    +	// /admin/teams
    +	r.HandleFunc("/admin/teams", nil)
    +	r.HandleFunc("/admin/teams/{team_id:[0-9]+}/{user_id:[0-9]+}", nil)
    
     	http.ListenAndServe(":8080", nil)
     }

--- backend/go.mod
+++ backend/go.mod
@@ -2,7 +2,10 @@ module github.com/SystemEngineeringTeam/ryuou-manager
 
 go 1.17
 
-require github.com/jinzhu/gorm v1.9.16
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+)
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"net/http"
 
-	"github.com/SystemEngineeringTeam/ryuou-manager/handler"
+	"github.com/gorilla/mux"
+	// "github.com/SystemEngineeringTeam/ryuou-manager/handler"
 )
 
 func main() {
-	http.HandleFunc("/questions", handler.QuestionHandler)
-	http.HandleFunc("/admin/questions", handler.AdminQuestionHandler)
-	http.HandleFunc("/admin/teams", handler.AdminTeamHandler)
-	http.HandleFunc("/users", handler.UserHandler)
-	http.HandleFunc("/users/login", handler.LoginHandler)
+	r := mux.NewRouter()
+
+	// /questions
+	r.HandleFunc("/questions/{team_id:[0-9]+}", nil)
+	r.HandleFunc("/questions/{team_id:[0-9]+}/{question_id:[0-9]+}", nil)
+
+	// /users
+	r.HandleFunc("/users", nil)
+	r.HandleFunc("/users/login", nil)
+
+	// /admin/questions
+	r.HandleFunc("/admin/questions", nil)
+	r.HandleFunc("/admin/questions/{question_id:[0-9]+}", nil)
+	r.HandleFunc("/admin/questions/{team_id:[0-9]+}/{question_id:[0-9]+}", nil)
+
+	// /admin/teams
+	r.HandleFunc("/admin/teams", nil)
+	r.HandleFunc("/admin/teams/{team_id:[0-9]+}/{user_id:[0-9]+}", nil)
 
 	http.ListenAndServe(":8080", nil)
 }
