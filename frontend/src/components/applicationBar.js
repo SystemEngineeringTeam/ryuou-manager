e5007cc828d89c092eeed3a39e6b35626829f956
SuperTikuwa <tikuwamk2@gmail.com>
2021-12-18 06:45:52 +0900

    add: appbar編集

--- frontend/src/components/applicationBar.js
+++ frontend/src/components/applicationBar.js
@@ -10,20 +10,15 @@ import {
 } from "@mui/material";
 import { Menu } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const ApplicationBar = () => {
+  const [cookies] = useCookies();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" style={{ marginBottom: "20px" }}>
         <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            size="large"
-          >
-            <Menu />
-          </IconButton>
           <Typography
             variant="h6"
             color="inherit"
@@ -33,12 +28,31 @@ const ApplicationBar = () => {
           >
             竜王戦
           </Typography>
-          <Button color="inherit" component={Link} to="/signup">
-            登録
-          </Button>
-          <Button color="inherit" component={Link} to="/login">
-            ログイン
-          </Button>
+          {cookies.token !== "" ? (
+            <div>
+              <Button color="inherit" component={Link} to="/list">
+                問題一覧
+              </Button>
+              <Button color="inherit" component={Link} to="/ranking">
+                ランキング
+              </Button>
+              <Button
+                color="inherit"
+                href="https://github.com/SystemEngineeringTeam/ryuou-manager"
+              >
+                GitHub
+              </Button>
+            </div>
+          ) : (
+            <div>
+              <Button color="inherit" component={Link} to="/signup">
+                登録
+              </Button>
+              <Button color="inherit" component={Link} to="/login">
+                ログイン
+              </Button>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
