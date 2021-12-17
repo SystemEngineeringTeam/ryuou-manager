2f475482c37adca2534df9690920b8cb85e805f3
SuperTikuwa <tikuwamk2@gmail.com>
2021-12-18 03:43:02 +0900

    fix: APIのパラメータ修正

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,6 +5,8 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Signup from "./users/signup";
 import Login from "./users/login";
 import QuestionInfo from "./questions/list/questionInfo";
+import TeamCreate from "./admin/team/teamCreate";
+import TeamDivision from "./admin/team/teamDivision";
 import CreateForm from "./admin/questions/CreateForm";
 import SubmitList from "./admin/questions/SubmitList";
 
@@ -17,6 +19,8 @@ const App = () => {
         <Route path="signup" element={<Signup />} />
         <Route path="login" element={<Login />} />
         <Route path="questionInfo/:id" element={<QuestionInfo />} />
+        <Route path="admin/team/create" element={<TeamCreate />} />
+        <Route path="admin/team/division" element={<TeamDivision />} />
         <Route path="admin/questions/create" element={<CreateForm />} />
         <Route path="admin/submits" element={<SubmitList />} />
       </Routes>
